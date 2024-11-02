f2ffd0195cabb0117ec0fb58eec1ea5a87127bac
Alpha23-coder <alfredopachecolizares23@gmail.com>
2024-11-02 01:56:54 -0500

    Daño con enemigo cerca hecho

---
+++ Assets/Scripts/Enemy/EnemyDamage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private HudManager hudManager; // Referencia al script de HudManager
+    [SerializeField] private Transform player; // Referencia al jugador
+    [SerializeField] private float attackRange = 2.0f; // Distancia mínima para atacar
+    [SerializeField] private int damageAmount = 5; // Daño a aplicar al jugador
+    [SerializeField] private float attackCooldown = 1.5f; // Cooldown entre ataques
+
+    private bool canAttack = true;
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= attackRange && canAttack)
+        {
+            StartCoroutine(ApplyDamageWithCooldown());
+        }
+    }
+
+    private IEnumerator ApplyDamageWithCooldown()
+    {
+        canAttack = false;
+        hudManager.ApplyDamage(damageAmount); // Llama a ApplyDamage en HudManager
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
+}
